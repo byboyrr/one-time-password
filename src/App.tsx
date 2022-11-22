8f06a656f988792291e326c9c7849c78395067ee
byboyrr <rarespausan@yahoo.com>
2022-11-22 20:31:22 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { CheckPassword } from "./components/CheckPassword";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState<string>("");
-
+  console.log('test push');
   if (!currentUser) return <UsernameLogin setCurrentUser={setCurrentUser} />;
   else
     return (
